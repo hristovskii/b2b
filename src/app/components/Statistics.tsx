68850fa34e49281c0e6e5784923aed57e8764ce7
Novica Paunovski <124404839+NovicaPaunovski@users.noreply.github.com>
2024-12-04 20:19:37 +0100

    Fixed participants spelling

--- src/app/components/Statistics.tsx
+++ src/app/components/Statistics.tsx
@@ -8,7 +8,7 @@ export default function Statistics() {
         <section className="my-10 md:mx-28 mx-12">
             <RevealList interval={100} delay={500}>
             <div className="lg:flex justify-evenly items-center gap-24">
-                <Numbers number={"350+"} text={"participants"}></Numbers>
+                <Numbers number={"350+"} text={"Participants"}></Numbers>
                 <Numbers number={"10+"} text={"Speakers"}></Numbers>
                 <Numbers number={"3"} text={"Successful Events"}></Numbers>
             </div>
