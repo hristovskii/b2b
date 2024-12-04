8afaf2ea22f15910a69b8e6824ce22043f1b4920
Petar Hristovski <petarhristovski23@gmail.com>
2024-12-04 17:59:49 +0100

    adding changes

---
+++ src/app/components/Statistics.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import Numbers from "./Numbers";
+import { RevealList } from  'next-reveal'
+
+export default function Statistics() {
+    return(
+        <section className="my-10 md:mx-28 mx-12">
+            <RevealList interval={100} delay={500}>
+            <div className="lg:flex justify-evenly items-center gap-24">
+                <Numbers number={"350+"} text={"participants"}></Numbers>
+                <Numbers number={"10+"} text={"Speakers"}></Numbers>
+                <Numbers number={"3"} text={"Successful Events"}></Numbers>
+            </div>
+            </RevealList>
+        </section>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "tailwindcss";
 
-export default {
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +8,28 @@ export default {
   ],
   theme: {
     extend: {
+      // backgroundImage: {
+      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+      //   "gradient-conic":
+      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      // },
+      fontFamily: {
+        eestec: ['Roboto', 'sans-serif']
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        eestec: "#fbbe22"
+      },
+      backgroundColor: {
+        eestec: "#fbbe22"
+      },
+      fill: {
+        eestec: "#fbbe22"
+      },
+      border: {
+        eestec: "#fbbe22"
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
+export default config;
